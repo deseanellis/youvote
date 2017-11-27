008fa668a44c38b4de3c753ca331ed4230fe8db5
DeSean Ellis <deseanellis@hotmail.com>
2017-11-27 16:40:25 -0400

    Content Change

--- index.js
+++ index.js
@@ -49,9 +49,12 @@ if (process.env.NODE_ENV === 'production') {
   //Express will serve production assets
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get('/', (req, res) => {
+    res.send('Home');
   });
+  /*app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });*/
 } else {
   app.use(express.static('client/public'));
 }
