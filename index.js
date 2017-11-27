75c9aecbb77bcfbfac2932cfb072b1ef65abfc0a
DeSean Ellis <deseanellis@hotmail.com>
2017-11-27 19:39:07 -0400

    Testing server routes

--- index.js
+++ index.js
@@ -49,18 +49,16 @@ require('./routes/validation')(app);
 
 if (process.env.NODE_ENV === 'production') {
   //Express will serve production assets
-  //app.use(express.static('client/build'));
-
-  app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-} else {
-  app.use(express.static('client/public'));
+} /*
+else {
+ app.use(express.static('client/public'));
 }
+*/
 //Listener and Port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
