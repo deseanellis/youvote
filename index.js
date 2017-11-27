34075c9b3659c9dde47b49cec9d9516d9f449add
DeSean Ellis <deseanellis@hotmail.com>
2017-11-27 16:59:32 -0400

    Content Change

--- index.js
+++ index.js
@@ -45,16 +45,19 @@ require('./routes/auth')(app);
 require('./routes/poll')(app);
 require('./routes/validation')(app);
 
+app.get('/', (req, res) => {
+  res.send({ home: 'here' });
+});
+
 if (process.env.NODE_ENV === 'production') {
   //Express will serve production assets
+  /*
   app.use(express.static('client/build'));
 
-  app.get('/', (req, res) => {
-    res.send('Home');
-  });
-  /*app.get('*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });*/
+  });
+  */
 } else {
   app.use(express.static('client/public'));
 }
