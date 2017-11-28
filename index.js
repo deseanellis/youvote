0d287b08af9964206afcde8286359744ea72fff8
DeSean Ellis <deseanellis@hotmail.com>
2017-11-28 13:06:49 -0400

    Removed static.json

--- index.js
+++ index.js
@@ -49,9 +49,9 @@ if (process.env.NODE_ENV === 'production') {
   //Express will serve production assets
   app.use(express.static('client/build'));
 
-  /*app.get('*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });*/
+  });
 } else {
   app.use(express.static('client/public'));
 }
