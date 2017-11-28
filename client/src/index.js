c124f38804de38b712fa4367966fe4ad6f8bab31
DeSean Ellis <deseanellis@hotmail.com>
2017-11-28 14:16:32 -0400

    Unregistered Service Worker

--- client/src/index.js
+++ client/src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { unregister } from './registerServiceWorker';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
-import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
-window.axios = require('axios');
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
 );
-//registerServiceWorker();
+//Un-register Service Worker
+unregister();
